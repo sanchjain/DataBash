a15e2b6cc108cffb729efcbaf44ccb79cd5292b4
Sanchit Jain <sanchjain101@gmail.com>
2021-10-04 14:38:47 +0530

    secret key removed

--- databash/databash/settings.py
+++ databash/databash/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tv)n)ycv5l#3g8x9q7ok$_6wgd=j$j7ehoe)_&)(w^(4b0z_oj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
